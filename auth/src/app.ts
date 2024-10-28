56676d3d2e7d4878f04f0d4abc8927f0a8375a2f
RAJENDRA JONDHALEKAR <jondhalekarrajendra02@gmail.com>
2024-10-28 13:37:59 +0530

    Testing microservice Architeture

---
+++ auth/src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import 'express-async-errors'
+import {json} from 'body-parser'
+import cookieSession from 'cookie-session';
+
+import { currrentUserRouter, signinUserRouter, signoutUserRouter, signuptUserRouter } from './routes';
+import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(cookieSession({
+    signed: false,
+    secure: true,
+}))
+app.use(currrentUserRouter);
+app.use(signinUserRouter);
+app.use(signoutUserRouter);
+app.use(signuptUserRouter);
+app.all('*', ()=> {
+    throw new NotFoundError();
+})
+app.use(errorHandler);
+
+export { app }
